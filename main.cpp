32db9c720df7244d37caff1b15ef50e74235a0a8
Boros Eliza Georgiana <eliza.georgiana0920@gmail.com>
2026-03-19 15:28:16 +0200

    Add project structure

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+std::ifstream fin ("date.in");
+std::ofstream fout ("date.out");
+class bautura {
+    ///denumire
+    ///pret preparare
+    ///reteta de unde mostenesc ingrediente
+    ///+ingrediente extra
+    ///timp de preparare
+    ///constructor explicit bautura
+    ///constructor de copiere
+    ///operatorul=
+    ///destructorul
+    ///functie pentru pretul final in care adaug pret preparare+pretul fiecarui ingredient
+    ///functie pentru reducere procent
+    ///verifica disponibilitae
+};
+class ingredient {
+    ///cantitate pe stoc
+    ///nume ingredient
+    ///pret ingredient client
+    ///pret ingredient cu care a fost cumparat
+    ///atribute private accesibile prin getters
+    ///functie de suplimentare stoc
+    ///adaugare ingrediente
+    ///categorii vegane, non-vegane
+    ///sugar free
+
+};
+class comanda {
+    ///numar comanda
+    ///lista produse comandate
+    ///indice-in perparare, nu se poate prepara-stoc indisponibil, finalizare comanda
+    ///operator de afisare a comenzii care sa includ bautura, ingrediente plus specificatii extra, pret=bon
+    ///functie de verificare disponibilitate
+    ///tips?
+    ///timp de preparare
+};
+
+///++generare de raport zilinic cu inasari =profit etc.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
