02e60fcaa03746d84b28301a554849e1f3c6409f
Boros Eliza Georgiana <eliza.georgiana0920@gmail.com>
2026-03-21 20:33:13 +0200

    Am adaugat clasa bautura, impreuna cu fisierul de intrare retete.txt

--- main.cpp
+++ main.cpp
@@ -10,43 +10,82 @@ private:
     float pret;
     bool vegan;
     bool faraZahar;
+    float kcal;
 
 public:
     ///Constructor implicit
-    Ingredient():nume(), stoc(), pret(), vegan(), faraZahar() {}
+    Ingredient():nume(nullptr), stoc(0), pret(0.0f), vegan(false), faraZahar(false), kcal(0.0f) {}
 
-    ///Constructor principal
-    Ingredient(const char* nume_, int stoc_, float pret_, bool vegan_, bool faraZahar_):stoc{stoc_}, pret{pret_}, vegan{vegan_}, faraZahar{faraZahar_} {
-        nume=new char[strlen(nume_)+1];
-        strcpy(nume, nume_);
+    ///Constructor de initializare
+    Ingredient(const char* nume_, int stoc_, float pret_, bool vegan_, bool faraZahar_, float kcal_)
+    :stoc{stoc_}, pret{pret_}, vegan{vegan_}, faraZahar{faraZahar_}, kcal{kcal_} {
+        if (nume_) {
+            nume=new char[std::strlen(nume_)+1];
+            std::strcpy(nume, nume_);
+        }
+        else
+            nume=nullptr;
+
     }
 
-    ///Constructor cu un ingur parametru
-    Ingredient (const char *nume): stoc(), pret(), vegan(), faraZahar() {
-        this->nume=new char[strlen(nume)+1];
-        strcpy(this->nume, nume);
-    }
-
-    ///Constructor de copiere
-    Ingredient(const Ingredient& other):stoc(other.stoc), pret(other.pret), vegan(other.vegan), faraZahar(other.faraZahar) {
-        if (other.nume) {
-            nume=new char[strlen(other.nume)+1];
-            strcpy(nume, other.nume);
+    ///Constructor cu un singur parametru
+    explicit Ingredient (const char *nume_): stoc(0), pret(0.0f), vegan(false), faraZahar(false), kcal(0.0f) {
+        if (nume_) {
+            nume=new char[std::strlen(nume_)+1];
+            std::strcpy(nume, nume_);
         }
         else
             nume=nullptr;
     }
 
-    void cresterPret() {
-        if (this->stoc<500 && this->stoc >0) {
-            float pretVechi=this->pret;
-            this->pret*=1.20;
-            std::cout<<"Stoc limitat de "<<this->nume<<"! Pretul a fost marit: "<<pretVechi<<" ==> "<<this->pret<<"\n";
+    ///Constructor de copiere (CC)
+    Ingredient(const Ingredient& other):stoc(other.stoc), pret(other.pret), vegan(other.vegan), faraZahar(other.faraZahar), kcal(other.kcal) {
+        if (other.nume) {
+            nume=new char[std::strlen(other.nume)+1];
+            std::strcpy(nume, other.nume);
+        }
+        else
+            nume=nullptr;
+    }
+
+    ///Operator de copiere (op=)
+    Ingredient& operator=(const Ingredient& other) {
+        ///eliberez memoria
+        if (this!=&other) {
+            delete[] nume;
+
+            ///copiez datele vechi
+            stoc=other.stoc;
+            pret=other.pret;
+            vegan=other.vegan;
+            faraZahar=other.faraZahar;
+            kcal=other.kcal;
+
+            ///aloc spatiu si copiez noul nume
+            if (other.nume) {
+                nume=new char[std::strlen(other.nume)+1];
+                std::strcpy(nume, other.nume);
+            }
+            else
+                nume=nullptr;
+        }
+        return *this;
+
+    }
+
+    ///Destructor
+    ~Ingredient() {
+        delete[] nume;
+    }
+
+    void cresterePret() {
+        if (stoc>0 && stoc<500) {
+            pret+=pret*12/100;
         }
     }
 
     void reaprovizionare(int stocAdaugat) {
-        this->stoc+=stocAdaugat;
+        stoc+=stocAdaugat;
     }
 
     //Getteri
@@ -70,45 +109,228 @@ public:
     {
         return faraZahar;
     }
-
-    ///Destructor
-    ~Ingredient() {
-      ///  delete[] nume;
+    float getKcal() const
+    {
+        return kcal;
     }
 
+    void setNume(const char* numeNou) {
+        if (numeNou) {
+            delete[] nume;
+            nume=new char[strlen(numeNou)+1];
+            strcpy(nume, numeNou);
+        }
+    }
     ///Friend pentru operatori
     friend std::istream& operator>>(std::istream& is, Ingredient& ing) {
-        char copie[100];
-        is>>copie>>ing.stoc>>ing.pret>>ing.vegan>>ing.faraZahar;
+        char copie[300];
+        if (is>>copie>>ing.stoc>>ing.pret>>ing.vegan>>ing.faraZahar>>ing.kcal){
         delete[] ing.nume;
-        ing.nume=new char[strlen(copie)+1];
-        strcpy(ing.nume, copie);
+        ing.nume=new char[std::strlen(copie)+1];
+        std::strcpy(ing.nume, copie);
+    }
         return is;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Ingredient& ing) {
-        os<<"Nume: " << ing.nume <<", stoc: " <<ing.stoc <<", pret: "<<ing.pret<<", vegan: "<<ing.vegan<<", fara zahar :"<<ing.faraZahar<<"\n";
+        os<<"Nume: " << ing.nume <<", stoc: " <<ing.stoc <<", pret: "<<ing.pret<<", vegan: "<<ing.vegan<<", fara zahar :"<<ing.faraZahar<<", kcal:"<<ing.kcal<<"\n";
         return os;
     }
 };
 
-class bautura {
-    std::string denumire;
-    int pret;
+class Bautura {
+    char *numeBautura;
+    float pretPreparare;
+    int timpPreparare;
+    Ingredient* listaIngrediente;
+    int nrIngrediente;
 
-    ///denumire
-    ///pret preparare
-    ///reteta care contine elemente de tip ingredient
-    ///+ingrediente extra
-    ///timp de preparare
-    ///constructor explicit bautura
-    ///constructor de copiere
-    ///operatorul=
-    ///destructorul
-    ///functie pentru pretul final in care adaug pret preparare+pretul fiecarui ingredient
-    ///functie pentru reducere procent oferta vegan, daca toate sunt vegane atunci aplic o reducere
-    ///verifica disponibilitae
+public:
+    ///Constructor Implicit
+    Bautura():numeBautura(nullptr), pretPreparare(0.0f), timpPreparare(0), listaIngrediente(nullptr), nrIngrediente(0) {}
+
+    ///Constructor de initializare
+    Bautura(const char* numeBautura_, float pretPreparare_, int timpPreparare_): pretPreparare{pretPreparare_}, timpPreparare{timpPreparare_}, listaIngrediente(nullptr), nrIngrediente(0)
+    {
+        if (numeBautura_)
+        {
+            numeBautura = new char[std::strlen(numeBautura_) + 1];
+            std::strcpy(numeBautura, numeBautura_);
+        }
+        else numeBautura = nullptr;
+    }
+
+    ///Constructor de copiere(CC)
+    Bautura(const Bautura& other)
+        :pretPreparare(other.pretPreparare), timpPreparare(other.timpPreparare), nrIngrediente(other.nrIngrediente)
+    {
+        if (other.numeBautura)
+        {
+            numeBautura = new char[std::strlen(other.numeBautura) + 1];
+            std::strcpy(numeBautura, other.numeBautura);
+        }
+        else numeBautura = nullptr;
+
+        if (other.listaIngrediente)
+        {
+            listaIngrediente = new Ingredient[other.nrIngrediente];
+            for (int i=0; i<other.nrIngrediente; i++)
+                listaIngrediente[i] = other.listaIngrediente[i]; // Se bazează pe op= din Ingredient
+        }
+        else listaIngrediente = nullptr;
+    }
+
+    ///Operator de copiere(op=)
+    Bautura& operator=(const Bautura& other)
+    {
+        ///eliberez memoria
+        if (this!=&other)
+        {
+            delete[] numeBautura;
+            delete[] listaIngrediente;
+
+            ///copiez datele vechi
+            pretPreparare=other.pretPreparare;
+            timpPreparare=other.timpPreparare;
+            nrIngrediente=other.nrIngrediente;
+
+            ///aloc spatiu si copiez noul nume
+            if (other.numeBautura)
+            {
+                numeBautura = new char[std::strlen(other.numeBautura) + 1];
+                std::strcpy(numeBautura, other.numeBautura);
+            }
+            else numeBautura = nullptr;
+
+            if (other.listaIngrediente)
+            {
+                listaIngrediente = new Ingredient[other.nrIngrediente];
+                for (int i=0; i<other.nrIngrediente; ++i)
+                {
+                    listaIngrediente[i] = other.listaIngrediente[i]; // Se bazează pe op= din Ingredient
+                }
+            }
+            else listaIngrediente = nullptr;
+        }
+        return *this;
+
+    }
+    ///Destructor
+    ~Bautura()
+    {
+        delete[] numeBautura;
+        delete[] listaIngrediente;
+    }
+
+    ///Getteri
+    const char* getNumeBautura() const
+    {
+        return numeBautura;
+    }
+    int getTimpPreparare() const
+    {
+        return timpPreparare;
+    }
+    float getPretPreparare() const
+    {
+        return pretPreparare;
+    }
+
+    int getNrIngrediente() const
+    {
+        return nrIngrediente;
+    }
+
+
+    float calculeazaPret() const
+    {
+        float pretTotal=0;
+        for(int i=0; i<nrIngrediente; i++)
+        {
+            pretTotal+=listaIngrediente[i].getPret();
+        }
+        return pretTotal;
+    }
+
+    float calculeazaKcal() const
+    {
+        float kcalTotal=0;
+        for(int i=0; i<nrIngrediente; i++)
+        {
+            kcalTotal+=listaIngrediente[i].getKcal();
+        }
+        return kcalTotal;
+    }
+
+    bool verificaVegan() const
+    {
+        bool ok=true;
+        for(int i=0; i<nrIngrediente; i++)
+        {
+            if(listaIngrediente[i].getVegan()==0)
+                ok=false;
+        }
+        return ok;
+    }
+
+    double aplicareReducere20() const
+    {
+        double pretRedus=calculeazaPret();
+        if(verificaVegan()==true)
+            pretRedus-=pretRedus*20/100;
+
+        return pretRedus;
+    }
+
+    bool verificaStoc()
+    {
+        bool ok=true;
+        for(int i=0; i<nrIngrediente; i++)
+        {
+            if(listaIngrediente[i].getStoc()==0)
+                ok=false;
+        }
+        return ok;
+    }
+
+
+    ///Friend pentru operatori
+    friend std::istream& operator>>(std::istream& is, Bautura& bau)
+    {
+        char copie[300];
+        if(is>>copie>>bau.pretPreparare>>bau.timpPreparare>>bau.nrIngrediente)
+        {
+            delete[] bau.numeBautura;
+            bau.numeBautura=new char[std::strlen(copie)+1];
+            std::strcpy(bau.numeBautura, copie);
+
+            delete[] bau.listaIngrediente;
+            bau.listaIngrediente=new Ingredient[bau.nrIngrediente];
+
+            for(int i=0; i<bau.nrIngrediente; i++)
+            {
+                char numeIng[100];
+                is>>numeIng;
+
+                bau.listaIngrediente[i].setNume(numeIng);
+            }
+        }
+
+        return is;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Bautura& bau)
+    {
+        os<<"Nume: " << bau.numeBautura <<", pret preparare: " <<bau.pretPreparare<<", timp preparare: "<<bau.timpPreparare<<", lista de ingrediente: ";
+        for(int i=0; i<bau.nrIngrediente-1; i++)
+        {
+            os<<bau.listaIngrediente[i].getNume()<<", ";
+        }
+        os<<bau.listaIngrediente[bau.nrIngrediente-1].getNume()<<"\n";
+        return os;
+    }
 };
+
 
 class comanda {
     ///numar comanda
@@ -118,11 +340,10 @@ class comanda {
     ///functie de verificare disponibilitate
     ///tips?
     ///timp de preparare, furnizare ora la care este gata comanda
+    ///ingrediente extra
 };
 
-class barista {
-    ///
-};
+
 
 ///++generare de raport zilinic cu inasari =profit etc.
 int main() {
